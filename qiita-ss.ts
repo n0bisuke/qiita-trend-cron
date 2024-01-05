389a84053bd31c4c97a57f0fc345c677dcf743b1
n0bisuke <rsksound@gmail.com>
2024-01-05 16:27:32 +0000

    code update

--- qiita-ss.ts
+++ qiita-ss.ts
@@ -1,17 +1,56 @@
 import puppeteer from 'puppeteer';
 import {setTimeout} from "node:timers/promises";
 
+import fs from 'node:fs';
+const QIITA_URL = 'https://qiita.com'; //ログインしてないとホームがトレンドになる
+const SS_FILE_PATH = 'screenshot.png';
+
+//Discord Webhook Post
+const discordPost = async (filePath = `screenshot.png`) => {
+    const DiscordWebhookURL = process.env.DISCORD_WEBHOOK_URL;
+
+    //送信するデータ1
+    const payload = {
+        username: 'n0bisuke BOT',
+        content: 'Node.js Fetch APIからポスト'
+    };
+
+    try {
+        const file = fs.readFileSync(filePath)
+        const formData = new FormData()
+        formData.append('file', new Blob([file], { type: 'image/png' }), 'file.png')
+        formData.append("payload_json", JSON.stringify(payload));
+
+        const response = await fetch(DiscordWebhookURL, {
+            method: 'POST',
+            body: formData
+        });
+    
+        return response.json();
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
 (async () => {
+
     const browser = await puppeteer.launch({
         headless: 'new',
+        args: ['--lang=ja']
     });
       
     const page = await browser.newPage();
-    await page.goto('https://developer.chrome.com/');
+    await page.goto(QIITA_URL);
     // 表示まで少し待機
     await setTimeout(1000);
-    await page.screenshot({ path: 'screenshot.png', fullPage: true })
-    
+    await page.screenshot({ path: SS_FILE_PATH, fullPage: true });
+    console.log('Screenshot saved!');
+
+    await setTimeout(1000);
+    const res = await discordPost(SS_FILE_PATH);
+    console.log(res);
+
     await browser.close();
 }
 )();
